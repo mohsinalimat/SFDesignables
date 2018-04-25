60f96fa2f2d74ffa46e154c5b1ba6ec3b9b17089
sudofluff <sudofluff@icloud.com>
2018-04-25 08:08:11 -0700

    init

---
+++ sfdesignables/sfdesignables.h
@@ -0,0 +1,19 @@
+//
+//  sfdesignables.h
+//  sfdesignables
+//
+//  Created by sudofluff on 4/25/18.
+//  Copyright © 2018 sudofluff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sfdesignables.
+FOUNDATION_EXPORT double sfdesignablesVersionNumber;
+
+//! Project version string for sfdesignables.
+FOUNDATION_EXPORT const unsigned char sfdesignablesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sfdesignables/PublicHeader.h>
+
+
